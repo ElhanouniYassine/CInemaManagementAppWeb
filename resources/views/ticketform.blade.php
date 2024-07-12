8c68265e0dc700e6ecad9b77e655343a22234815
ElhanouniYassine <J138138880@um5.ac.ma>
2024-07-12 21:39:06 +0100

    Commit

--- resources/views/ticketform.blade.php
+++ resources/views/ticketform.blade.php
@@ -9,7 +9,7 @@
 
     <div style="display: flex;justify-content: left ;width: 100%;gap: 2rem;">
     <p style="font-family: sans-serif ;margin: 0%"> time:10.15</p><br>
-    <p style="font-family: sans-serif;margin: 0%">seat:A02</p>
+    <p style="font-family: sans-serif;margin: 0%">seat:A03</p>
 
     </div>
     </div>
